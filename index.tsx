1aaadb8824d4332278d5f6e38cf7cbd916d1fb82
xiaogounaodai-crypto <xiaogounaodai@gmail.com>
2026-03-02 02:00:06 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -84,3 +84,15 @@ export const getIconComponent = (iconName: string, className?: string) => {
     default: return <Utensils {...props} />;
   }
 };
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
